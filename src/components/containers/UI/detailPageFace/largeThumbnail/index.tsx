0417d5b9da3a40ec91b97ea6300049b8db57b49d
tranquangtien025 <tranquangtien025@gmail.com>
2022-09-05 20:20:55 +0700

    update

--- src/components/containers/UI/detailPageFace/largeThumbnail/index.tsx
+++ src/components/containers/UI/detailPageFace/largeThumbnail/index.tsx
@@ -1,10 +1,11 @@
-import { StyledImage, StyledThumbnail } from "./styles"
+import { StyledImage, StyledImageDiv, StyledThumbnail } from "./styles"
 
 export const LargeThumbnail = (props: any) => {
   
   return (
     <StyledThumbnail>
       <StyledImage src={props.largeThumbnail} />
+      <StyledImageDiv />
     </StyledThumbnail>
   )
 }
